41887adf4f487cab49679fc10955c4e671ab8aef
PC2408 <applelgy@naver.com>
2024-12-26 15:01:36 +0900

    임시 저장

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   return (
     <>
       <div>
+      <div class="touch-device">터치가 가능합니다</div>
+      <div class="untouch-device">마우스, 터치패드 인터렉션이 가능합니다</div>
 
       </div>
     </>
