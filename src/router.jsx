c4cbc1cb8ec59d120cf4e8aa0d84a7b87117a1da
PC2408 <applelgy@naver.com>
2025-01-02 17:39:40 +0900

    Fix: router 확인

--- src/router.jsx
+++ src/router.jsx
@@ -3,14 +3,6 @@ import BoardHome from "./features/board/BoardHome";
 import MainLayout from "./MainLayout";
 import BookSearch from "./features/record/pages/BookSerach";
 
-{/*function MainLayout() {
-  return (
-    <div id="app">
-      <h1>hello</h1>
-      <Outlet />
-    </div>
-  );
-}*/}
 
 export default createBrowserRouter([
   {
