cdd451c4b81987de5d1c8094963b2e24d9ecd385
PC2408 <applelgy@naver.com>
2025-01-02 15:05:20 +0900

    Fix: git Router 충돌 개선

--- src/router.jsx
+++ src/router.jsx
@@ -20,6 +20,8 @@ export default createBrowserRouter([
       {
         path: '/record',
         element: <BookSearch/>
+      },
+      {
         path: '/board',
         element: <BoardHome />
       }
