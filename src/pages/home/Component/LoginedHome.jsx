930c510a74c5e4ba944f1eab9710aa7025b3d5eb
Chaeri-1 <alfsdo03@hufs.ac.kr>
2025-01-19 20:00:46 +0900

    Refactor: 홈 폴더 구조 수정

---
+++ src/pages/home/Component/LoginedHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReviewContent from './ReviewContent'
+
+function LoginedHome() {
+  return (
+    <>
+      <h3>나의 최근 기록</h3>
+      <ReviewContent/>
+    </>
+  )
+}
+
+export default LoginedHome
---
+++ src/pages/home/Component/ReviewContent.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function ReviewContent() {
+  return (
+    <div>
+      <div>
+        <h4>하늘과 바람과 별과 시</h4>
+        <div>윤동주</div>
+      </div>
+      <button>읽는중</button>
+      <div>시작: 2024.09.21</div>
+      <div>
+        <img/>
+        <div>
+          <div>마지막 기록: 2024.09.30</div>
+          <h5>인상적이었던 구절</h5>
+          <br />
+          <div>나는 살아있다</div>
+          <h5>감상</h5>
+          <br />
+          <div>서시로 알게 된 윤동주 시인인데 생각보다 시가 다 이뻐서 너무 다 이뻐서 너무 재밌게 읽었다. 교과서에서만 봤던 내용들도 있고 윤동주라는 사람과 더 가까워지는...</div>
+          <div>
+            <button>더보기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewContent
