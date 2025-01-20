f15bf16ccea3cfc81c39478412a8f58227c52931
Chaeri-1 <alfsdo03@hufs.ac.kr>
2025-01-20 17:43:39 +0900

    회원가입

--- src/features/auth/signup/Signup.jsx
+++ src/features/auth/signup/Signup.jsx
@@ -48,8 +48,8 @@ function Signup() {
       console.log(formData)
       const response = await api.post("/member/register/", formData)
       console.log('회원가입 성공', response.data)
-      localStorage.setItem("access", response.data.access)
-      localStorage.setItem("setpk", response.data.user.id)
+      localStorage.setItem("access", response.tocken.access)
+      localStorage.setItem("setpk", response.data.userid)
       navigate('/home', {replace: true})
     } catch (error) {
       console.log('회원가입 중 오류: ', error)
